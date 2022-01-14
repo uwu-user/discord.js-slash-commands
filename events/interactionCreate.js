222721907815de0694b694f9c1b3c81ea5d16152
Unknown_ <71901584+uwu-user@users.noreply.github.com>
2022-01-14 04:23:56 +0300

    Add files via upload

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,1 +1,19 @@
-$~
+
+module.exports = {
+  name: "interactionCreate",
+  async execute(interaction) {
+    if (!interaction.isCommand()) return;
+    if (!interaction.inGuild()) return;
+    if (interaction.deferred) return;
+    if (!interaction.client.commands.has(interaction.commandName)) return;
+    try {
+      const { commandName } = interaction;
+      if (!interaction.client.commands.has(commandName)) return;
+      await interaction.client.commands
+        .get(commandName)
+        .execute(interaction.client, interaction);
+    } catch (error) {
+      await interaction.reply({ content: error.message, ephemeral: true });
+    }
+  },
+};
