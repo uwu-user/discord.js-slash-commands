a2dddf9b5add8879ae8abddb7f9899a3972ac07f
Unknown_ <71901584+uwu-user@users.noreply.github.com>
2022-01-14 04:21:59 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,147 +1,96 @@
 const { Serverid, TOKEN } = require("./info.js");
-const { Discord, Client, APIMessage, Intents } = require("discord.js");
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+const { Discord, Client, Intents, Collection } = require("discord.js");
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 
 const fs = require("fs");
-const ascii = require("ascii-table");
-const table = new ascii("slash Commands");
-const Commands = [];
-table.setHeading("Command", "status", "Area");
 
 //-------------------------------------------------------------
-// login
+// login ?
 //-------------------------------------------------------------
 
-client.login(TOKEN).catch(function(error) {
+client.login(TOKEN).catch(function (error) {
   console.log(error.message);
 });
-
-//-------------------------------------------------------------
-// Delete commands (automatic) :)
-//-------------------------------------------------------------
-
-// It works without problems. delete /* and */ if you want to delete all commands from your server and Global...
-
-/*
-client.on("ready", async () => {
-  let Globalcmds = await client.api.applications(client.user.id).commands.get();
-  let Guildcmds = await client.api
-    .applications(client.user.id)
-    .guilds(Serverid)
-    .commands.get();
-  Globalcmds.forEach(cmd => {
-    client.api
-      .applications(client.user.id)
-      .commands(cmd.id)
-      .delete();
-    console.log(
-      "Deleted Global command { name: " + cmd.name + ", ID: " + cmd.id + " }"
-    );
-  });
-  Guildcmds.forEach(cmds => {
-    client.api
-      .applications(client.user.id)
-      .guilds(Serverid)
-      .commands(cmds.id)
-      .delete();
-    console.log(
-      "Deleted Guild command { name: " + cmds.name + ", ID: " + cmds.id + " }"
-    );
-  });
-});
-*/
 
 //-------------------------------------------------------------
 // Bot Ready (add commands) :)
 //-------------------------------------------------------------
 
+const ascii = require("ascii-table");
+const table = new ascii("Handler's.js");
+
+let Handlersnum = 0;
+
 client.on("ready", async () => {
-  const commandFiles = fs
-    .readdirSync("./commands")
-    .filter(file => file.endsWith(".js"));
-  for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    Commands.push(command);
-    client.api
-      .applications(client.user.id)
-      .guilds(Serverid)
-      .commands.post({
-        data: {
-          name: command.name,
-          description: command.description,
-          options: command.options
-        }
-      });
-    if (command.global == true) {
-      client.api.applications(client.user.id).commands.post({
-        data: {
-          name: command.name,
-          description: command.description,
-          options: command.options
-        }
-      });
-    }
-    if (command.name) {
-      table.addRow(file, `[ ✔ ]`, `[${command.global ? "global" : "guild"}]`);
-    } else {
-      table.addRow(file, `[ ✖ ]`, `[Error]`);
-    }
-  }
-  // nodejs
-  const nodejs = `Node.js: [ ${process.version} ]`;
-  // npmjs
-  const npmjsv = require("./package.json").engines.npm || "Error";
-  const npmjs = `npm.js: [ ${npmjsv} ]`;
-  // djs
-  const { version: discordjsVersion } = require("discord.js");
-  const djs = `Discord.js: [ ${discordjsVersion} ]`;
-  // time
-  var currentTime = new Date(),
-    hours12 = currentTime.getHours() + 3,
-    hours24 = currentTime.getHours() + 3,
-    minutes = currentTime.getMinutes(),
-    seconds = currentTime.getSeconds(),
-    years = currentTime.getFullYear(),
-    month = currentTime.getMonth() + 1,
-    day = currentTime.getDate(),
-    week = currentTime.getDay(),
-    Suffix = "AM";
-
-  if (minutes < 10) {
-    minutes = "0" + minutes;
-  }
-
-  if (hours12 >= 12) {
-    Suffix = "PM";
-    hours12 = hours12 - 12;
-  }
-
-  if (commandFiles == "")
-    return console.log(
-      "✖ | There are no commands added!\nwhy not add one or more =3?"
-    );
-  if (!commandFiles == "") {
-    let Time12 = `「 ${years}/${month}/${day} 」|「 ${hours12}:${minutes}:${seconds} ${Suffix} 」`;
-    let Time24 = `「 ${years}/${month}/${day} 」|「 ${hours24}:${minutes}:${seconds} 」`;
-    console.log(
-      `\n\n\n[✔ | Loading...]\n\n${nodejs}\n${npmjs}\n${djs}\n\n${Time12}\n${Time24}\n` +
-        table.toString()
-    );
-  }
-});
-
-//-------------------------------------------------------------
-// Interaction
-//-------------------------------------------------------------
-
-client.on("interactionCreate", async interaction => {
   try {
-    const file = Commands.find(
-      cmd => cmd.name.toLowerCase() === interaction.commandName.toLowerCase()
-    );
-    if (file) file.execute(client, interaction, interaction.options);
-    //                     client  interaction  args
+    // handler's
+    const handlersFiles = fs
+      .readdirSync("./handlers")
+      .filter((file) => file.endsWith(".js"));
+
+    for (const file of handlersFiles) {
+      const handlers = require(`./handlers/${file}`);
+      if (!handlersFiles == "") {
+        ++Handlersnum;
+        table.addRow(`[ ✔ ] - ${file}`);
+      }
+    }
+    if (handlersFiles == "") {
+      table.addRow(`[ ✖ ] - NaN-404`);
+    }
+
+    // nodejs
+    const nodejs = `Node.js: [ ${process.version} ]`;
+    // npmjs
+    let npmjsv;
+
+    try {
+      npmjsv = require("./package.json").engines.npm;
+    } catch (error) {
+      npmjsv = undefined;
+    }
+
+    const npmjs = `npm.js: [ v${npmjsv} ]`;
+    // djs
+    const { version: discordjsVersion } = require("discord.js");
+    const djs = `Discord.js: [ v${discordjsVersion} ]`;
+
+    // time
+    var currentTime = new Date(),
+      hours12 = currentTime.getHours() + 3,
+      hours24 = currentTime.getHours() + 3,
+      minutes = currentTime.getMinutes(),
+      seconds = currentTime.getSeconds(),
+      years = currentTime.getFullYear(),
+      month = currentTime.getMonth() + 1,
+      day = currentTime.getDate(),
+      week = currentTime.getDay(),
+      Suffix = "AM";
+
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+
+    if (hours12 >= 12) {
+      Suffix = "PM";
+      hours12 = hours12 - 12;
+    }
+
+    if (handlersFiles) {
+      let Time12 = `「 ${years}/${month}/${day} 」|「 ${hours12}:${minutes}:${seconds} ${Suffix} 」`;
+      let Time24 = `「 ${years}/${month}/${day} 」|「 ${hours24}:${minutes}:${seconds} 」`;
+      let msg1 = `\n\n\n[✔ | Loading...]\n\n${nodejs}\n${npmjs}\n${djs}\n\n${Time12}\n${Time24}\n`;
+      let msg2 = table.toString();
+      let msg3 = "\n「 Handler's 」|「 " + Handlersnum + " 」";
+      console.log(msg1 + msg2 + msg3);
+    }
+    for (const file of handlersFiles) {
+      require(`./handlers/${file}`)(client);
+    }
   } catch (error) {
-    interaction.reply({ content: error.message, ephemeral: true });
+    console.error(error.message);
   }
 });
