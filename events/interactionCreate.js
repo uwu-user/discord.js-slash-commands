e9a8c630388a2c9cbb79e423e71ec0c3a7aaf0e7
Unknown_ <71901584+uwu-user@users.noreply.github.com>
2022-06-06 09:24:21 +0300

    dm, messages

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,8 +3,10 @@ module.exports = {
   name: "interactionCreate",
   async execute(interaction) {
     if (!interaction.isCommand()) return;
-    if (!interaction.inGuild()) return;
     if (!interaction.client.commands.has(interaction.commandName)) return;
+    if (!interaction.inGuild()) {
+      await interaction.reply({ content: "Error: This is not allowed in dm", ephemeral: true });
+    }
     try {
       const { commandName } = interaction;
       if (!interaction.client.commands.has(commandName)) return;
@@ -12,7 +14,7 @@ module.exports = {
         .get(commandName)
         .execute(interaction.client, interaction);
     } catch (error) {
-      await interaction.reply({ content: error.message, ephemeral: true });
+      await interaction.reply({ content: error.message || "Error", ephemeral: true });
     }
   },
 };
